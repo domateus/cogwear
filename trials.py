fa3adf9e5b57a9d335c183e6fabafcafcf52e2b1
Mateus Franceschina <mateusfranceschina@gmail.com>
2024-09-25 20:59:55 +0000

    more training

--- trials.py
+++ trials.py
@@ -6,11 +6,12 @@ from src.signals.eeg import EEGSubject
 import os
 from keras.api.backend import clear_session
 
-classifiers = ['fcn', 'lstm']
+classifiers = ['fcn', 'cnn', 'lstm', 'resnet']
 path = os.getcwd()
 
 for c in classifiers:
   print(c)
+  clear_session()
   # exp = PPGExperiment(classifier=c, type=ExperimentType.END_TO_END, path=path, device='samsung')
   # tuner = Tuner(exp)
   # tuner.tune(evals=20, max_evals=40)
@@ -19,19 +20,19 @@ for c in classifiers:
   # clear_session()
   # exp.test_best_models(tuner)
 
-  exp = PPGExperiment(classifier=c, type=ExperimentType.END_TO_END, path=path, device='empatica')
-  tuner = Tuner(exp)
+  # exp = PPGExperiment(classifier=c, type=ExperimentType.END_TO_END, path=path, device='empatica')
+  # tuner = Tuner(exp)
   # tuner.tune(evals=20, max_evals=40)
   # clear_session()
-  exp.losocv_run(tuner)
+  # exp.losocv_run(tuner)
   # clear_session()
   # exp.test_best_models(tuner)
 
-  # exp = EDAExperiment(classifier=c, type=ExperimentType.END_TO_END, path=path)
-  # tuner = Tuner(exp)
+  exp = EDAExperiment(classifier=c, type=ExperimentType.END_TO_END, path=path)
+  tuner = Tuner(exp)
   # tuner.tune(evals=14, max_evals=40)
   # clear_session()
-  # exp.losocv_run(tuner)
+  exp.losocv_run(tuner)
   # clear_session()
   # exp.test_best_models(tuner)
 
